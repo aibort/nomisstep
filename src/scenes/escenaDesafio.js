13a1783d91169b1fde815248176cfca2936b9b1e
aibort <aibort@ucm.es>
2019-12-13 17:25:44 +0100

    escenaDesafio merge

--- src/scenes/escenaDesafio.js
+++ src/scenes/escenaDesafio.js
@@ -1,17 +1,35 @@
+//import EscenaBase from "../scenes/escenaCreativa.js";
+import Player from "../gameObjects/player.js";
+import Road from        "../gameObjects/road.js";
+import BaseBlock from   "../gameObjects/baseBlock.js";
+import Tramp from       "../gameObjects/tramp.js";
+import Wall from        "../gameObjects/wall.js";
+import Trap from        "../gameObjects/tramp.js";
+
+
 export default class Challenger extends Phaser.Scene{
+    
     constructor(){
         super({key:'Challenger'}); 
+        this.player;
     }
     create(){
-
-    }
-
-            //Amaro: Esto irá en la escena desafío
-        //this.player  = new Player(this,POS_CAMINO_X ,POS_CAMINO_Y,"jugador").setScale(0.5);
-        //Crea la colisión entre el jugador y los distintos elementos 
-        //this.physics.add.collider(this.player,this.baseGroup,this.onCollision);
-        //this.physics.add.collider(this.player,this.trapGroup,this.onCollision);
-        //this.cameras.main.startFollow(this.player);
-        //this.cameras.main.setViewport(450, 200, 400, 400);
+        this.player  = new Player(this,POS_CAMINO_X ,POS_CAMINO_Y,"jugador").setScale(0.5);
+        //Crea la colisión entre el jugador y los distintos elementos
+        this.physics.add.collider(this.player,this.baseGroup);//,this.onCollision); // revisar onCollision
+        this.physics.add.collider(this.player,this.trapGroup,this.onCollision);
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.setViewport(450, 200, 400, 400);
+        //this.scene.load('Challenger',info);
+        //Phaser.Scene.call(,)
+        //cargar desde tablero, 
+        //trampa invisible como camino y con posibilidiad de desactivarla (sprite si es posible)
+    } 
+        onCollision(){
+            this.player.setPosition(this.sys.game.config.width/2, this.sys.game.config.height/2);
+        }
+       
 }
+
+
 
